e0604fee65a63412de05fe8079ea5ee3c102c544
jnuho <cactoos555@gmail.com>
2024-07-30 01:48:54 +0900

    .

--- script/1.minikube.sh
+++ script/1.minikube.sh
@@ -3,7 +3,7 @@
 docker context use default
 
 if ! minikube status | grep "Running" &> /dev/null; then
-    minikube start --kubernetes-version=v1.30.3 --driver=docker
+    minikube start --kubernetes-version=v1.30.3 --driver=docker --cpus 4 --memory 4g
 else
     echo "Minikube cluster is already running."
 fi
