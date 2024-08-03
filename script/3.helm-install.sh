1869af9e6b572b9c8b158ba1bfbd5fe65378a635
jnuho <cactoos555@gmail.com>
2024-08-03 14:20:22 +0900

    k8s context if-else script

--- script/3.helm-install.sh
+++ script/3.helm-install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+current_context=$(kubectl config current-context)
 
-if [ "$(kubectl config current-context)" = "minikube" ]; then
-    helm install tst-release ./tst-chart -f ./tst-chart/values.dev.yaml
-else
+if [ "$current_context" = "minikube" ]; then
+    helm install tst-release ./tst-chart -f ./tst-chart/values.local.yaml
+elif [ "$current_context" = "pi" ]; then
+    helm install tst-release ./tst-chart -f ./tst-chart/values.pi.yaml
+elif [[ "$current_context" == *"aws:eks"* ]]; then
     aws eks update-kubeconfig --region ap-northeast-2 --name my-cluster --profile terraform
-    helm install tst-release ./tst-chart -f ./tst-chart/values.prd.yaml
+    helm install tst-release ./tst-chart -f ./tst-chart/values.prd.AWS.L4.ingress.controller.yaml
+else
+    echo "NO KUBERNETES CONTEXT FOUND!"
 fi
--- script/4.helm-upgrade.sh
+++ script/4.helm-upgrade.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-if [ "$(kubectl config current-context)" = "minikube" ]; then
-    helm upgrade tst-release ./tst-chart -f ./tst-chart/values.dev.yaml
-else
+current_context=$(kubectl config current-context)
+
+if [ "$current_context" = "minikube" ]; then
+    helm upgrade tst-release ./tst-chart -f ./tst-chart/values.local.yaml
+elif [ "$current_context" = "pi" ]; then
+    helm upgrade tst-release ./tst-chart -f ./tst-chart/values.pi.yaml
+elif [[ "$current_context" == *"aws:eks"* ]]; then
     aws eks update-kubeconfig --region ap-northeast-2 --name my-cluster --profile terraform
-    helm upgrade tst-release ./tst-chart -f ./tst-chart/values.prd.yaml
+    helm upgrade tst-release ./tst-chart -f ./tst-chart/values.prd.AWS.L4.ingress.controller.yaml
+else
+    #echo "NO KUBERNETES CONTEXT FOUND!"
 fi
+
